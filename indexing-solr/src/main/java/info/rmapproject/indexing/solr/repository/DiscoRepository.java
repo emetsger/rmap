0c6a1e2988d9374a4bb7be5c91058aaf882f1ed5
Elliot Metsger <emetsger@jhu.edu>
2017-07-31 15:43:36 -0400

    Add domain-specific query to repository, use camel case for field name in domain document instead of using underscore.

--- indexing-solr/src/main/java/info/rmapproject/indexing/solr/repository/DiscoRepository.java
+++ indexing-solr/src/main/java/info/rmapproject/indexing/solr/repository/DiscoRepository.java
@@ -3,9 +3,14 @@ package info.rmapproject.indexing.solr.repository;
 import info.rmapproject.indexing.solr.model.DiscoSolrDocument;
 import org.springframework.data.solr.repository.SolrCrudRepository;
 
+import java.net.URI;
+import java.util.Set;
+
 /**
  * @author Elliot Metsger (emetsger@jhu.edu)
  */
 public interface DiscoRepository extends SolrCrudRepository<DiscoSolrDocument, Long> {
+
+    Set<DiscoSolrDocument> findDiscoSolrDocumentsByDiscoAggregatedResourceUris(URI discoAggregatedResourceUri);
 
 }
