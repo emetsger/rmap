a65aa341cdec976137fed1334333e4c84e1617bc
Elliot Metsger <emetsger@jhu.edu>
2017-07-31 15:45:45 -0400

    More simple tests with spring data 3.x, including using query creation strategy in domain repository interface.

--- indexing-solr/src/test/java/info/rmapproject/indexing/solr/SimpleSolrTest.java
+++ indexing-solr/src/test/java/info/rmapproject/indexing/solr/SimpleSolrTest.java
@@ -4,19 +4,27 @@ import info.rmapproject.indexing.solr.model.DiscoSolrDocument;
 import info.rmapproject.indexing.solr.repository.DiscoRepository;
 import org.apache.solr.client.solrj.response.SolrPingResponse;
 import org.apache.solr.client.solrj.response.UpdateResponse;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.solr.core.DefaultQueryParser;
 import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.Query;
+import org.springframework.lang.Nullable;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.net.URI;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -29,8 +37,7 @@ import static org.junit.Assert.assertTrue;
 public class SimpleSolrTest {
 
     @Autowired
-    @Qualifier("solrTemplate")
-    private SolrTemplate solrTemplateWithCore;
+    private SolrTemplate solrTemplate;
 
     @Autowired
     private DiscoRepository discoRepository;
@@ -39,9 +46,8 @@ public class SimpleSolrTest {
      * Fails: can't specify a core name to ping
      */
     @Test
-    @Ignore
     public void testPing() throws Exception {
-        SolrPingResponse res = solrTemplateWithCore.ping();
+        SolrPingResponse res = solrTemplate.ping();
         assertNotNull(res);
         assertTrue(res.getElapsedTime() > 0);
         assertEquals(0, res.getStatus());
@@ -53,29 +59,13 @@ public class SimpleSolrTest {
     @Test
     @SuppressWarnings("unchecked")
     public void simpleWrite() throws Exception {
+        DiscoSolrDocument doc = discoDocument(1, "simpleWriteWithTemplate");
 
-        DiscoSolrDocument doc = new DiscoSolrDocument();
-        doc.setDisco_id(5L);
-        doc.setDisco_uri(URI.create("http://rmapproject.org/disco/1234"));
-        doc.setDisco_creator_uri(URI.create("http://foaf.org/Elliot_Metsger"));
-        doc.setDisco_aggregated_resource_uris(new ArrayList() {
-            {
-                add("http://doi.org/10.1109/disco.test");
-                add("http://ieeexplore.ieee.org/example/000000-mm.zip");
-            }
-        });
-        doc.setDisco_provenance_uri(URI.create("http://rmapproject.org/prov/1234"));
-        doc.setDisco_related_statements(new ArrayList() {
-            {
-                add("TODO n3 triples");
-            }
-        });
-
-        // Don't need to use the saveBean(core, doc) method, because the document has the core as an annotation
-        UpdateResponse res = solrTemplateWithCore.saveBean(doc);
+        // NOT TRUE WITH spring-data-solr 3: Don't need to use the saveBean(core, doc) method, because the document has the core as an annotation
+        UpdateResponse res = solrTemplate.saveBean("discos", doc);
         assertNotNull(res);
 
-        solrTemplateWithCore.commit("discos");
+        solrTemplate.commit("discos");
     }
 
     /**
@@ -85,23 +75,7 @@ public class SimpleSolrTest {
      */
     @Test
     public void simpleWriteWithRepo() throws Exception {
-        DiscoSolrDocument doc = new DiscoSolrDocument();
-        doc.setDisco_id(10L);
-        doc.setDisco_description("simpleWriteWithRepo");
-        doc.setDisco_uri(URI.create("http://rmapproject.org/disco/5678f"));
-        doc.setDisco_creator_uri(URI.create("http://foaf.org/Elliot_Metsger"));
-        doc.setDisco_aggregated_resource_uris(new ArrayList() {
-            {
-                add("http://doi.org/10.1109/disco.test");
-                add("http://ieeexplore.ieee.org/example/000000-mm.zip");
-            }
-        });
-        doc.setDisco_provenance_uri(URI.create("http://rmapproject.org/prov/5678"));
-        doc.setDisco_related_statements(new ArrayList() {
-            {
-                add("TODO n3 triples");
-            }
-        });
+        DiscoSolrDocument doc = discoDocument(10, "simpleWriteWithRepo");
 
         DiscoSolrDocument saved = discoRepository.save(doc);
         assertNotNull(saved);
@@ -109,23 +83,7 @@ public class SimpleSolrTest {
 
     @Test
     public void simpleCountAndDelete() throws Exception {
-        DiscoSolrDocument doc = new DiscoSolrDocument();
-        doc.setDisco_id(20L);
-        doc.setDisco_description("simpleCountAndDelete");
-        doc.setDisco_uri(URI.create("http://rmapproject.org/disco/5678f"));
-        doc.setDisco_creator_uri(URI.create("http://foaf.org/Elliot_Metsger"));
-        doc.setDisco_aggregated_resource_uris(new ArrayList() {
-            {
-                add("http://doi.org/10.1109/disco.test");
-                add("http://ieeexplore.ieee.org/example/000000-mm.zip");
-            }
-        });
-        doc.setDisco_provenance_uri(URI.create("http://rmapproject.org/prov/5678"));
-        doc.setDisco_related_statements(new ArrayList() {
-            {
-                add("TODO n3 triples");
-            }
-        });
+        DiscoSolrDocument doc = discoDocument(20, "simpleCountAndDelete");
 
         DiscoSolrDocument saved = discoRepository.save(doc);
         assertNotNull(saved);
@@ -136,4 +94,80 @@ public class SimpleSolrTest {
 
         assertEquals(0, discoRepository.count());
     }
+
+    @Test
+    public void testSimpleFindUsingDocumentId() throws Exception {
+        List<Long> ids = Arrays.asList(100L, 101L, 102L);
+        ids.stream().map(id -> discoDocument(id, "testSimpleSearchUsingDocumentId"))
+                .forEach(doc -> discoRepository.save(doc));
+
+        Set<Long> found = StreamSupport.stream(
+                discoRepository.findAllById(ids).spliterator(), false)
+                .map(DiscoSolrDocument::getDisco_id)
+                .collect(Collectors.toSet());
+
+        ids.forEach(expectedId -> assertTrue(found.stream().anyMatch(expectedId::equals)));
+        assertTrue(found.stream().noneMatch(id -> (id < 100 || id > 102)));
+    }
+
+    @Test
+    public void testSimpleFindUsingCriteria() throws Exception {
+        DefaultQueryParser queryParser = new DefaultQueryParser();
+        queryParser.registerConverter(new Converter<URI, String>() {
+            @Nullable
+            @Override
+            public String convert(URI uri) {
+                if (uri == null) {
+                    return null;
+                }
+                String converted = uri.toString().replaceAll(":", "\\\\:");
+//                System.err.println("Converted '" + uri + "' to '" + converted + "'");
+                return converted;
+            }
+        });
+        solrTemplate.registerQueryParser(Query.class, queryParser);
+        List<Long> ids = Arrays.asList(200L, 201L, 202L);
+        ids.stream().map(id -> discoDocument(id, "testSimpleFindUsingCriteria"))
+                .forEach(doc -> discoRepository.save(doc));
+
+        Set<DiscoSolrDocument> found = discoRepository
+                .findDiscoSolrDocumentsByDiscoAggregatedResourceUris(URI.create("http://doi.org/10.1109/disco.test"));
+
+        assertNotNull(found);
+
+        Set<DiscoSolrDocument> filtered = found.stream()
+                .filter(doc -> (doc.getDisco_id() > 199 && doc.getDisco_id() < 300))
+                .collect(Collectors.toSet());
+
+        assertEquals(3, filtered.size());
+        assertTrue(filtered.stream().allMatch(doc -> doc.getDisco_id() >= 200 && doc.getDisco_id() < 203));
+    }
+
+    /**
+     * Domain instance document
+     * @param id
+     * @param testDescription
+     * @return
+     */
+    private static DiscoSolrDocument discoDocument(long id, String testDescription) {
+        DiscoSolrDocument doc = new DiscoSolrDocument();
+        doc.setDisco_description(testDescription);
+        doc.setDisco_id(id);
+        doc.setDisco_uri(URI.create("http://rmapproject.org/disco/5678f"));
+        doc.setDisco_creator_uri(URI.create("http://foaf.org/Elliot_Metsger"));
+        doc.setDiscoAggregatedResourceUris(new ArrayList() {
+            {
+                add("http://doi.org/10.1109/disco.test");
+                add("http://ieeexplore.ieee.org/example/000000-mm.zip");
+            }
+        });
+        doc.setDisco_provenance_uri(URI.create("http://rmapproject.org/prov/5678"));
+        doc.setDisco_related_statements(new ArrayList() {
+            {
+                add("TODO n3 triples");
+            }
+        });
+        return doc;
+    }
+
 }
