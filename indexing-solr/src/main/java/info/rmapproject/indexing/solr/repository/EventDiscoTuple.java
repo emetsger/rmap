e7c9bbeeb4e2ade53801ee9ee2376a63c69e6267
Elliot Metsger <emetsger@jhu.edu>
2017-11-02 09:42:06 -0400

    Javadoc, make class public as it is exposed by the EventTupleIndexingRepository.

--- indexing-solr/src/main/java/info/rmapproject/indexing/solr/repository/EventDiscoTuple.java
+++ indexing-solr/src/main/java/info/rmapproject/indexing/solr/repository/EventDiscoTuple.java
@@ -5,20 +5,50 @@ import info.rmapproject.core.model.RMapStatus;
 import info.rmapproject.core.model.agent.RMapAgent;
 import info.rmapproject.core.model.disco.RMapDiSCO;
 import info.rmapproject.core.model.event.RMapEvent;
+import info.rmapproject.indexing.solr.model.DiscoSolrDocument;
 
 /**
- * A decomposed instance of a {@link IndexDTO} into tuples keyed by the event and disco.
+ * Represents a unique pairing of an {@link RMapEvent} and {@link RMapDiSCO}.  An instance of {@code EventDiscoTuple}
+ * {@link EventDiscoTupleMapper maps} one-to-one with a {@link DiscoSolrDocument Solr document}.  An {@code
+ * EventDiscoTuple} carries additional information describing the event and DiSCO.  This includes information about the
+ * {@link RMapAgent agent} that is associated with the event, the {@link RMapStatus status} of the DiSCO, and IRIs
+ * referencing the source and target of the event.
  * <p>
- * A {@code IndexDTO} instance will decompose to at least one (at most two) {@code EventDiscoTuple} instance; one
- * tuple for the {@code (Event, Event Source)} and one tuple for the {@code (Event, Event Target)}.  Event source
- * and targets are typically DiSCOs.  Practically speaking, each instance of a {@code EventDiscoTuple} is
- * represented by a Solr document in the index.
+ * A {@link IndexDTO} instance will decompose to at least one (at most two) {@code EventDiscoTuple} instance; one
+ * tuple for the {@code (Event, Source DiSCO)} and one tuple for the {@code (Event, Target DiSCO)} (Event source
+ * and targets are DiSCOs).  Practically speaking, each instance of a {@code EventDiscoTuple} is represented by a Solr
+ * document in the index.
+ * </p>
  */
-class EventDiscoTuple {
+public class EventDiscoTuple {
+
+    /**
+     * The event, which has {@link #disco the disco} as a source or target
+     */
     RMapEvent event;
+
+    /**
+     * The disco, which is either the source or target of the {@link #event}
+     */
     RMapDiSCO disco;
+
+    /**
+     * The agent, which is associated with {@link #event}
+     */
     RMapAgent agent;
+
+    /**
+     * The status of the {@link #disco}
+     */
     RMapStatus status;
+
+    /**
+     * The IRI to the object considered the source of the {@link #event}
+     */
     RMapIri eventSource;
+
+    /**
+     * The IRI to the object considered the target of the {@link #event}
+     */
     RMapIri eventTarget;
 }
