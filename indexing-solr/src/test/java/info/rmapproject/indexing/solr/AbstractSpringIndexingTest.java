813344f0cd9d48a371f95eb61cb9d1f93cc6e15f
Karen Hanson <karenhanson77@gmail.com>
2017-12-18 19:13:26 -0500

    Implement embedded-solr for tests

--- indexing-solr/src/test/java/info/rmapproject/indexing/solr/AbstractSpringIndexingTest.java
+++ indexing-solr/src/test/java/info/rmapproject/indexing/solr/AbstractSpringIndexingTest.java
@@ -1,24 +1,24 @@
 package info.rmapproject.indexing.solr;
 
-import info.rmapproject.core.rdfhandler.RDFHandler;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import info.rmapproject.core.rdfhandler.RDFHandler;
+
 /**
  * @author Elliot Metsger (emetsger@jhu.edu)
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ActiveProfiles({"default", "inmemory-triplestore", "inmemory-idservice", "inmemory-db", "http-solr", "mock-kafka"})
-@ContextConfiguration({"classpath:/rmap-indexing-solr.xml", "classpath:/spring-rmapcore-context.xml", "classpath:/rmap-kafka-shared-test.xml"})
+@ActiveProfiles({"default", "inmemory-triplestore", "inmemory-idservice", "inmemory-db", "embedded-solr", "mock-kafka"})
+@ContextConfiguration({"classpath*:/rmap-indexing-solr.xml", "classpath*:/spring-rmapcore-context.xml", "classpath*:/rmap-kafka-shared-test.xml"})
 @TestPropertySource(properties = {"integration.db.rmap-agent-sql = classpath:/create-rmap-agent.sql"})
 public abstract class AbstractSpringIndexingTest {
 
@@ -32,7 +32,7 @@ public abstract class AbstractSpringIndexingTest {
     @Before
     public void setUp() throws Exception {
         if (System.getProperty("spring.profiles.active") == null) {
-            System.setProperty("spring.profiles.active", "default, integration-db, inmemory-triplestore, inmemory-idservice, http-solr, mock-kafka");
+            System.setProperty("spring.profiles.active", "default, integration-db, inmemory-triplestore, inmemory-idservice, embedded-solr, mock-kafka");
             thisClassSetProfilesProperty = true;
         }
     }
